5c9f13ef818a7bb336511a155b6d5cdf584ebabb
FE_강명구 <mg_@ex-em.com>
2023-02-18 15:31:28 +0900

    [fea] edit info page
    info page 수정

---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Card from '../../components/templates/Card.svelte';
+
+  let userId = 0;
+  let nickName = 'mykang';
+  let userProfileMessage = 'fucking exem';
+  // - 자신을 표현하는 글, 카톡 상태 메세지 같은 느낌
+  let iPAddress;
+  let userImage;
+</script>
+
+<Card>
+  <p slot="title">
+    { userId } { nickName }
+  </p>
+  <p slot="content">
+    { userProfileMessage }
+  </p>
+  <p slot="sub">
+    { iPAddress }
+  </p>
+</Card>
