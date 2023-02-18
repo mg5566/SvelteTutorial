5428c229c63a8451cb35607a9a975d76b3f9a7ef
FE_강명구 <mg_@ex-em.com>
2023-02-18 15:25:57 +0900

    [fea] add common components
    dividor 랑 card component 추가

---
+++ src/components/atoms/Dividor.ts
@@ -0,0 +1,2 @@
+export type Thick = 'small' | 'medium' | 'large';
+
---
+++ src/components/templates/Card.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Dividor from "../atoms/Dividor.svelte";
+</script>
+
+<div class="card-container">
+  <slot name="title"></slot>
+  <Dividor thick="medium" --track-color="black"/>
+  <slot name="content"></slot>
+</div>
+
+<style>
+  .card-container {
+      border: 2px solid var(--color-theme-1);
+      border-radius: 12px;
+      padding: 1rem;
+  }
+</style>
