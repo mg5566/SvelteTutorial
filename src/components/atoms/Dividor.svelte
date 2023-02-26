3f453e20aead55a7bb9ad0093583dbef5058cd2a
FE_강명구 <mg_@ex-em.com>
2023-02-26 17:15:22 +0900

    [fit] create dividor component
    dividor component 구현

---
+++ src/components/atoms/Dividor.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import type { Thick } from './Dividor.ts';
+
+  export let thick: Thick = 'small';  // small, medium, large
+
+  $: styledThick = () => {
+    if (thick === 'small') {
+      return '1px';
+    } else if (thick === 'medium') {
+      return '3px;'
+    } else if (thick === 'large') {
+      return '5px';
+    }
+    return '10px';
+  }
+</script>
+
+<div class="dividor" style="{`--styled-thick: ${styledThick()};`}"></div>
+
+<style>
+  .dividor {
+      padding: 0 1rem;
+      /*background-color: var(--color-theme-2);*/
+      background-color: var(--track-color);
+      /*height: 2px;*/
+      height: var(--styled-thick);
+  }
+</style>
